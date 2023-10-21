6f16ccd887f92b1fccbcb8b1f9d00ba68a437533
Rodrigo Martins <rodrigo.martins@cuboconnect.com>
2023-10-21 18:16:35 -0300

    feat: user service creation

---
+++ src/main/java/com/example/fastpayapi/fastpayapi/service/UserService.java
@@ -0,0 +1,35 @@
+package com.example.fastpayapi.fastpayapi.service;
+
+import com.example.fastpayapi.fastpayapi.domain.User.User;
+import com.example.fastpayapi.fastpayapi.domain.User.UserType;
+import com.example.fastpayapi.fastpayapi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public User saveUser(User user) {
+        return repository.save(user);
+    }
+
+    public User findUserById(Long id) throws Exception {
+        return repository.findUserById(id)
+                .orElseThrow(() -> new Exception("User nor found."));
+    }
+
+    public void validateTransaction(User payer, BigDecimal value) throws Exception {
+        if(payer.getUserType() == UserType.MERCHANT) {
+            throw new Exception("Only commom user can do transactions.");
+        }
+
+        if(payer.getAccountBalance().compareTo(value) < 0) {
+            throw new Exception("Payer do not have enough funds to do this transaction.");
+        }
+    }
+}
